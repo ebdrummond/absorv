41d82b9365725e50af596da25cea5954e8c38983
erindrummond <e.b.drummond@gmail.com>
2015-08-29 09:34:25 -0500

    Implement devise sign up, sign in, and sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
+  private
+
+  def after_sign_out_path_for(_resource_or_scope)
+    root_path
+  end
 end
