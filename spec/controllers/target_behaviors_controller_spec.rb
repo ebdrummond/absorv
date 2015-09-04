16ad046e52d34a7b94a10080ddc07e1cb0eaab9f
erindrummond <e.b.drummond@gmail.com>
2015-09-04 15:17:54 -0500

    Verify ability to view only own target behaviors

--- spec/controllers/target_behaviors_controller_spec.rb
+++ spec/controllers/target_behaviors_controller_spec.rb
@@ -6,8 +6,19 @@ describe TargetBehaviorsController do
   let(:user) { create(:user) }
 
   describe "GET index" do
-    before(:each) { get :index, user_id: user.id }
-    specify { expect(response).to render_template(:index) }
+    let!(:target_behavior) { create(:target_behavior, user: user) }
+
+    it "renders the index template" do
+      get :index, user_id: user.id
+      expect(response).to render_template(:index)
+    end
+
+    it "assigns @target_behaviors for the current user" do
+      create(:target_behavior, user: create(:user))
+
+      get :index, user_id: user.id
+      expect(assigns(:target_behaviors)).to match_array([target_behavior])
+    end
   end
 
   describe "GET new" do
@@ -17,11 +28,17 @@ describe TargetBehaviorsController do
 
   describe "POST create" do
     context "with required parameters" do
+      let(:params) { attributes_for(:target_behavior) }
+
       it "creates a new target behavior" do
-        expect { post :create,
-          user_id: user.id,
-          target_behavior: attributes_for(:target_behavior)
+        expect {
+          post :create, user_id: user.id, target_behavior: params
         }.to change { user.target_behaviors.count }.by(1)
+      end
+
+      it "redirects to target behaviors index" do
+        post :create, user_id: user.id, target_behavior: params
+        expect(response).to redirect_to(target_behaviors_path)
       end
     end
 
